fbdee16e50edd1821a8536c166f2958f9574147a
Costin Nita <costin.nita@voya.ai>
2021-07-28 16:36:27 +0300

    Add support for tests

---
+++ atm-service/src/test/java/atm/service/rest/web/AccountRestControllerIntTest.java
@@ -0,0 +1,13 @@
+package atm.service.rest.web;
+
+import atm.service.AbstractIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AccountRestControllerIntTest extends AbstractIntegrationTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
